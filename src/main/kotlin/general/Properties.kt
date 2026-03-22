4f718745a252ead86567ec4fe737c784f4d635d1
Vorontsova <annreznik134@gmail.com>
2026-03-22 16:46:29 +0300

    added param tests

---
+++ src/main/kotlin/general/Properties.kt
@@ -0,0 +1,40 @@
+package org.example.general
+
+import java.util.Properties
+import kotlin.jvm.java
+
+object Config {
+
+    private const val DEFAULT_PROP_FILE = "/example.properties"
+
+    data class Props(
+        val browserName: String,
+        val browserVersion: String,
+        val frontendUrl: String,
+        val backendUrl: String,
+        val backendApiVersion: String,
+        val moonHost: String,
+    )
+
+    //Загрузка из пропертис
+    //by lazy - инициализируется только при первом обращении
+    val get: Props by lazy {
+        //динамическая конфигурация, если передали -Denv_config=/prod.properties, то возьмет его.
+        // если нет - то файл DEFAULT_PROP_FILE
+        val fileName = System.getProperty("env_config") ?: DEFAULT_PROP_FILE
+        val properties = Properties().apply {
+            val stream = Config::class.java.getResourceAsStream(fileName)
+                ?: throw IllegalStateException("Properties file '$fileName' not found")
+            stream.use {load(it)}
+        }
+        //маппинг
+        Props(
+            browserName = properties.getProperty("browser.name"),
+            browserVersion = properties.getProperty("browser.version"),
+            frontendUrl = properties.getProperty("frontend.url"),
+            backendUrl = properties.getProperty("backend.url"),
+            backendApiVersion = properties.getProperty("backend.api.version"),
+            moonHost = properties.getProperty("moon.host")
+        )
+    }
+}
---
+++ src/test/kotlin/PropertiesTests.kt
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+
+class PropertiesTests {
+
+    @Test
+    @DisplayName("Проверка загрузки свойств из файла")
+    fun checkPropertiesLoading() {
+      //  println("Property file: ${Config.Props.}")
+
+    }
+}
