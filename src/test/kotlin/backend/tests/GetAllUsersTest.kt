31f43f9e894c3001754ea1176b236de00329de3c
Vorontsova <annreznik134@gmail.com>
2026-05-03 01:23:57 +0300

    15 lesson HW fixes

--- src/test/kotlin/backend/tests/GetAllUsersTest.kt
+++ src/test/kotlin/backend/tests/GetAllUsersTest.kt
@@ -4,8 +4,10 @@ import backend.api.helpers.AuthHelper
 import backend.api.helpers.UserHelper
 import backend.api.models.users.createUser.randomUser
 import backend.controllers.Controllers
+import backend.extension.ResponseExt.Companion.checkIsSuccessful
 import backend.extension.ResponseExt.Companion.getAsObject
 import io.kotest.matchers.collections.shouldContain
+import io.kotest.matchers.equals.shouldBeEqual
 import io.kotest.matchers.ints.shouldBeLessThanOrEqual
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.DisplayName
@@ -17,11 +19,12 @@ class GetAllUsersTest: Controllers() {
     private val userHelper = UserHelper()
 
     private val authHelper = AuthHelper()
-    private val limit = 10
-    val bigLimit = 400
-    val zeroLimit = 0
+    private val myLimit = 10
+    private val limit = 50
+    private val bigLimit = 400
+    private val zeroLimit = 0
 
-    val bigOffset=400
+    private val bigOffset=400
 
     @Test
     @DisplayName("Get requested user from all users")
@@ -37,10 +40,10 @@ class GetAllUsersTest: Controllers() {
     fun limitRestrictsSize(){
         userHelper.ensureAmountOfUsers(60)
 
-        val page = user.getAllUsers(offset = 0, limit = limit)
+        val page = user.getAllUsers(offset = 0, limit = myLimit)
             .getAsObject()
 
-        page.size shouldBeLessThanOrEqual limit
+        page.size shouldBeLessThanOrEqual myLimit
         println(page.size)
 
         val pageOver = user.getAllUsers(offset = 0, limit = bigLimit)
@@ -50,12 +53,12 @@ class GetAllUsersTest: Controllers() {
     }
 
     @Test
-    @DisplayName("Check that pages is not overlaping")
+    @DisplayName("Check that pages is not overlap")
     fun usersPagesIsNotOverlap(){
         userHelper.ensureAmountOfUsers(25)
 
-        val page1 = user.getAllUsers(offset = 0, limit = limit).getAsObject()
-        val page2 = user.getAllUsers(offset = 1, limit = limit).getAsObject()
+        val page1 = user.getAllUsers(offset = 0, limit = myLimit).checkIsSuccessful().getAsObject()
+        val page2 = user.getAllUsers(offset = 1, limit = myLimit).checkIsSuccessful().getAsObject()
 
         val ids1 = page1.map { it.id }.toSet()
         val ids2 = page2.map { it.id }.toSet()
@@ -63,6 +66,7 @@ class GetAllUsersTest: Controllers() {
         (ids1 intersect ids2).isEmpty() shouldBe true
     }
 
+    //падает - 200
     @Test
     @DisplayName("Validation offset and limit parameters")
     fun negativeOffsetCheck(){
@@ -73,7 +77,7 @@ class GetAllUsersTest: Controllers() {
         val zeroLimitPage = user.getAllUsers(offset = 0, limit = zeroLimit)
         zeroLimitPage.code() shouldBe 400
 
-        val bigOffset = user.getAllUsers(offset = bigOffset, limit = limit)
+        val bigOffset = user.getAllUsers(offset = bigOffset, limit = myLimit)
         bigOffset.code() shouldBe 400
     }
 
@@ -81,12 +85,15 @@ class GetAllUsersTest: Controllers() {
     @DisplayName("Validation offset and limit size check")
     fun negativeOffsetCheckSize(){
         val bigLimitPage = user.getAllUsers(offset = 0, limit = bigLimit).getAsObject()
-        bigLimitPage.size shouldBeLessThanOrEqual 50
+        bigLimitPage.size shouldBeLessThanOrEqual limit
+        println(bigLimitPage.size)
 
         val zeroLimitPage = user.getAllUsers(offset = 0, limit = zeroLimit).getAsObject()
-        zeroLimitPage.size shouldBeLessThanOrEqual 50
+        zeroLimitPage.size shouldBeEqual 0
+        println(zeroLimitPage.size)
 
-        val bigOffset = user.getAllUsers(offset = bigOffset, limit = limit).getAsObject()
+        val bigOffset = user.getAllUsers(offset = bigOffset, limit = myLimit).getAsObject()
         bigOffset shouldBe emptyList()
+        println(bigOffset.size)
     }
 }
